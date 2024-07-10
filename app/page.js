6d07206292ecd704a6b3b38af82781b8a0665e95
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2024-07-10 21:14:07 +0330

    edited version

--- app/page.js
+++ app/page.js
@@ -1,36 +1,43 @@
 'use client'
+import { useEffect, useState } from 'react';
 
-import { useState } from "react";
+const Home = () => {
+  const [otp, setOTP] = useState('');
 
-export default function Home() {
-  const [otp,setOtp] = useState(0)
-  useState(() => {
-    if ("OTPCredential" in window) {
-      const ac = new AbortController();
-    
-      navigator.credentials
-        .get({
-          otp: { transport: ["sms"] },
-          signal: ac.signal
-        })
-        .then((otp) => {
-          setOtp(otp.code);
+  useEffect(() => {
+    const fetchOTP = async () => {
+      if ("OTPCredential" in window) {
+        const ac = new AbortController();
+
+        try {
+          const otpCredential = await navigator.credentials.get({
+            otp: { transport: ['sms'] },
+            signal: ac.signal
+          });
+          setOTP(otpCredential.code);
           ac.abort();
-        })
-        .catch((err) => {
+        } catch (err) {
           ac.abort();
-          console.log(err);
-        });
-    }
-  },[])
+          console.error('Error fetching OTP:', err);
+        }
+      }
+    };
 
+    fetchOTP();
+
+    return () => {
+      // Cleanup function
+      // Abort any ongoing fetch operation if component unmounts
+      console.log('Component unmounted');
+    };
+  }, []); // Empty dependency array ensures useEffect runs only once on mount
 
   return (
-    <>
-<div>OTP : {otp}</div>
-<input/>
-<button onClick ={() => console.log('hi bro')} style={{width : "3rem"}}>hi</button>
-
-    </>
+    <div className="App">
+      <h1>Hello Next.js</h1>
+      <h2>Your OTP is: {otp}</h2>
+    </div>
   );
-}
+};
+
+export default Home;
