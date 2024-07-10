474fb1307b20eb2a3b4b9ab4a0fdb92067932d8a
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2024-07-10 21:08:32 +0330

    start version of auto otp

--- app/page.js
+++ app/page.js
@@ -4,7 +4,27 @@ import { useState } from "react";
 
 export default function Home() {
   const [otp,setOtp] = useState(0)
-  useState(() => {},[])
+  useState(() => {
+    if ("OTPCredential" in window) {
+      const ac = new AbortController();
+    
+      navigator.credentials
+        .get({
+          otp: { transport: ["sms"] },
+          signal: ac.signal
+        })
+        .then((otp) => {
+          setOtp(otp.code);
+          ac.abort();
+        })
+        .catch((err) => {
+          ac.abort();
+          console.log(err);
+        });
+    }
+  },[])
+
+
   return (
     <>
 <div>OTP : {otp}</div>
